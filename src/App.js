5f65e2c9bf22989744b8a0e81e7358a25e8e9474
Ankit <brownankit1@gmail.com>
2023-12-22 20:41:42 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./App.css";
 
 function App() {
+  const [userMessage, setUserMessage] = useState('');
+  const [botResponse, setBotResponse] = useState('');
+
+  const handleSendMessage = async () => {
+    try {
+      const response = await axios.post('http://localhost:8080/api/chat', { message: userMessage });
+      setBotResponse(response.data.message);
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Simple Chatbot</h1>
+      <div>
+        <div>
+          <input
+            type="text"
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)}
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+        <div>
+          <p>User: {userMessage}</p>
+          <p>Bot: {botResponse}</p>
+        </div>
+      </div>
     </div>
   );
 }
